4c9aaf68edb24074cb9b932472305a70b1579ccc
unknown <nunof.h.fontes@gmail.com>
2018-08-27 23:35:02 +0100

    new modules, new configs

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+//require express
 const express = require('express');
 
+//initialize express
 const app = express();
 const port = process.env.PORT || 5000;
 
+//routes
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
 
+//Listening to requests
 app.listen(port, () => console.log(`Listening on port ${port}`));
