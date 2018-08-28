52d70070155efb9e55acd8679b97ab0adf806210
unknown <nunof.h.fontes@gmail.com>
2018-08-28 14:00:51 +0100

    updates to project: bcrypt, mongoose

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 //require express
 const express = require('express');
+//require file system
+var fileSystem = require('fs');
 
 //initialize express
 const app = express();
@@ -8,6 +10,21 @@ const port = process.env.PORT || 5000;
 //routes
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
+});
+
+//first page
+app.get('/login', (req, res) => {
+  fileSystem.readFile('./index.html', function(error, fileContent){
+		if(error){
+			res.writeHead(500, {'Content-Type': 'text/plain'});
+			res.end('Error');
+		}
+		else{
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.write(fileContent);
+			res.end();
+		}
+	});
 });
 
 //Listening to requests
